f8d7087bc80a8f29559d9053ce5ec1dad1ae206d
vikas <v8kgsm@gmail.com>
2020-04-16 10:54:03 +0530

    Dependency injection work.

---
+++ web application 5/app.js
@@ -0,0 +1,28 @@
+(function () {
+'use strict';
+
+angular.module('DIApp', [])
+.controller('DIController', DIController);
+
+function DIController ($scope,
+                       $filter,
+                       $injector) {
+  $scope.name = "Vikas";
+
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
+  console.log($injector.annotate(DIController));
+}
+
+function AnnonateMe(name, job, blah) {
+  return "Blah!";
+}
+
+console.log(DIController.toString());
+console.log(AnnonateMe());
+console.log(AnnonateMe);
+console.log(AnnonateMe.toString());
+})();
