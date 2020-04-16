936c4682b315a1c779d9dcab5ff63fb978fbd857
vikas <v8kgsm@gmail.com>
2020-04-16 11:22:42 +0530

    Dependency injection work.

--- web application 5/app.js
+++ web application 5/app.js
@@ -1,28 +1,29 @@
-(function () {
-'use strict';
-
-angular.module('DIApp', [])
-.controller('DIController', DIController);
-
-function DIController ($scope,
-                       $filter,
-                       $injector) {
-  $scope.name = "Vikas";
-
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
-  };
-
-  console.log($injector.annotate(DIController));
-}
-
-function AnnonateMe(name, job, blah) {
-  return "Blah!";
-}
-
-console.log(DIController.toString());
-console.log(AnnonateMe());
-console.log(AnnonateMe);
-console.log(AnnonateMe.toString());
-})();
+// (function () {
+// 'use strict';
+//
+// angular.module('DIApp', [])
+// .controller('DIController', DIController);
+//
+// function DIController ($scope,
+//                        $filter,
+//                        $injector) {
+//   $scope.name = "Vikas";
+//
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+//
+//   console.log($injector.annotate(DIController));
+// }
+//
+// function AnnonateMe(name, job, blah) {
+//   return "Blah!";
+// }
+//
+// console.log(DIController.toString());
+// console.log(AnnonateMe());
+// console.log(AnnonateMe);
+// console.log(AnnonateMe.toString());
+// })();
+!function(){"use strict";function e(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
