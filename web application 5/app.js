c56f2e217619536a3c78e479c310bd4e3936ed68
vikas <v8kgsm@gmail.com>
2020-04-16 11:45:44 +0530

    Dependency injection work.

--- web application 5/app.js
+++ web application 5/app.js
@@ -1,12 +1,10 @@
-// (function () {
+// first way 
+//(function () {
 // 'use strict';
 //
 // angular.module('DIApp', [])
-// .controller('DIController', DIController);
-//
-// function DIController ($scope,
-//                        $filter,
-//                        $injector) {
+// .controller('DIController', ['$scope','$filter',DIController]);
+// function DIController ($scope, $filter) {
 //   $scope.name = "Vikas";
 //
 //   $scope.upper = function () {
@@ -26,5 +24,27 @@
 // console.log(AnnonateMe);
 // console.log(AnnonateMe.toString());
 // })();
+
+
+// second Way
+
+// (function () {
+// 'use strict';
+//
+// angular.module('DIApp', [])
+// .controller('DIController', DIController);
+//
+// DIController.$inject = ['$scope', '$filter'];
+// function DIController($scope, $filter) {
+//   $scope.name = "Yaakov";
+//
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }
+//
+// })();
+
 //magnified code
 !function(){"use strict";function e(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
